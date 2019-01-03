76578cdb19c6ef0df901c96c6193131c28c8ff41
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2019-01-03 17:13:40 +0200

    Add installation of python3 and munch package

--- DreamFactory_Fedora.sh
+++ DreamFactory_Fedora.sh
@@ -604,6 +604,18 @@ then
         fi
 fi
 
+### INSTALL PYTHON3 MUNCH
+dnf install -y python3 python3-pip
+pip3 list | grep munch
+if (( $? >= 1 ))
+then
+        pip3 install munch
+        if (( $? >= 1 ))
+        then
+                echo -e  "${RD}\nCould not install python3 munch extension.${NC}" >&5
+        fi
+fi
+
 ### Install Node.js
 node -v
 if (( $? >= 1 ))
